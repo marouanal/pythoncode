15239fea1bf3f7152c726b3f32b7ba2e1285b6aa
Marouan <115103579+marouanal@users.noreply.github.com>
2023-03-28 15:04:47 +0100

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,15 +1,15 @@
-def word_freq(sentence, n):
-    words = sentence.split()
-    freq_dict = {}
-    for word in words:
-        if word in freq_dict:
-            freq_dict[word] += 1
+def word_frequency(sentence, n):
+    allwords = sentence.split()
+    frequency_list = {}
+    for word in allwords:
+        if word in frequency_list:
+            frequency_list[word] += 1
         else:
-            freq_dict[word] = 1
-    sorted_freq = sorted(freq_dict.items(), key=lambda x: (-x[1], x[0]))
-    return sorted_freq[:n]
+            frequency_list[word] = 1
+    sorted_frequency = sorted(frequency_list.items(), key=lambda x: (-x[1], x[0]))
+    return sorted_frequency[:n]
  
 sentence = "baz bar foo foo zblah zblah zblah baz toto bar"
 n = 3
-result = word_freq(sentence, n)
+result = word_frequency(sentence, n)
 print(result)
