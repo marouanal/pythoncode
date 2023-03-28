af11fc48edc3cfa6b8fd41a1c9a995b557fbd8f3
Marouan <115103579+marouanal@users.noreply.github.com>
2023-03-28 15:47:29 +0000

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,19 +1,3 @@
-#######################################    First Code    #############################################################
-def word_frequency(sentence, n):
-    allwords = sentence.split()
-    frequency_list = {}
-    for word in allwords:
-        if word in frequency_list:
-            frequency_list[word] += 1
-        else:
-            frequency_list[word] = 1
-    sorted_frequency = sorted(frequency_list.items(), key=lambda x: (-x[1], x[0]))
-    return sorted_frequency[:n]
- 
-sentence = "baz bar foo foo zblah zblah zblah baz toto bar"
-n = 3
-result = word_frequency(sentence, n)
-print(result)
 
 #######################################    Updated Code    #############################################################
 
